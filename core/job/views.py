c437dd7cfb8a71a73d091b2cbe03d0ee4c97e2d6
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2023-03-07 23:46:21 +0330

    23:46

--- core/job/views.py
+++ core/job/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from .models.job import Job
 from home.models import Slider
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 def listview(request):
+    contact_list = Job.objects.filter(status=True).order_by('-created'),
+    paginator = Paginator(contact_list, 25)
+    
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
     context = {
         'jobs':Job.objects.filter(status=True).order_by('-created'),
         'slider':Slider.objects.filter().last(),
