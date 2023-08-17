f12576f15242ef561e5191cd7236bae41720ba13
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2023-08-17 18:36:46 +0330

    update

--- core/core/settings.py
+++ core/core/settings.py
@@ -80,15 +80,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'usersdb', 
-        'USER': 'sammy', 
-        'PASSWORD': 'masoud416',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
